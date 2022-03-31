c5c2752e2098f8f1322adc900c66130797800c2e
MiroslavBurda <35538660+MiroslavBurda@users.noreply.github.com>
2022-03-31 16:52:22 +0200

    writing and reading work

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 #include "esp_err.h"
 #include "freertos/FreeRTOS.h"
 #include <freertos/task.h>
-#include <driver/gpio.h>
-#include "time.hpp"
-#include "stopwatch.hpp"
 #include <driver/i2c.h>
 
 #define RETURN_IF_ERR(x) do {                                         \
@@ -14,25 +11,14 @@
         }                                                               \
     } while(0)
 
-#define GPIO_OUTPUT_PIN_SEL (1ULL<<GPIO_NUM_17 )
-void iopins_init(void) {
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    gpio_config(&io_conf);
-}
-
-gpio_num_t sda_pin = GPIO_NUM_32;
-gpio_num_t scl_pin = GPIO_NUM_33; 
+gpio_num_t sda_pin = GPIO_NUM_18;
+gpio_num_t scl_pin = GPIO_NUM_19; 
 uint32_t speed_hz = 100000;
 uint8_t address = 0x15;
 i2c_port_t bus_num = I2C_NUM_0; 
 uint8_t DataToSend[] = {1, 2, 3, 4};
 size_t len = sizeof(DataToSend);
-
+uint8_t DataToRead[] = {3, 3, 3, 3};
 
 i2c_config_t conf = {
     .mode = I2C_MODE_MASTER,
@@ -46,38 +32,53 @@ i2c_config_t conf = {
     .clk_flags = 0, 
 };
 
-
 esp_err_t sendData(const uint8_t *data, size_t len )
 {
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     RETURN_IF_ERR(i2c_master_start(cmd));
-    RETURN_IF_ERR(i2c_master_write_byte(cmd, address << 1, true));
+    RETURN_IF_ERR(i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE, true));
     RETURN_IF_ERR(i2c_master_write(cmd, data, len, I2C_MASTER_LAST_NACK));
     RETURN_IF_ERR(i2c_master_stop(cmd));
     RETURN_IF_ERR(i2c_master_cmd_begin(bus_num, cmd, pdMS_TO_TICKS(125)));
+    i2c_cmd_link_delete(cmd);
     return ESP_OK;
 }
 
+esp_err_t readData(uint8_t *data, size_t len )
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    RETURN_IF_ERR(i2c_master_start(cmd));
+    RETURN_IF_ERR(i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_READ, true));
+    RETURN_IF_ERR(i2c_master_read(cmd, data, len, I2C_MASTER_LAST_NACK));
+    RETURN_IF_ERR(i2c_master_stop(cmd));
+    RETURN_IF_ERR(i2c_master_cmd_begin(bus_num, cmd, pdMS_TO_TICKS(125)));
+    i2c_cmd_link_delete(cmd);
+    return ESP_OK;
+}
 
-timeout send_data { msec(1000) }; // timeout zajistuje posilani dat do PC kazdych 1000 ms
-bool L_G = true;
 int i = 0;
 
 extern "C" void app_main() {  // example for connect ESP32 with I2C
-    iopins_init();
-    gpio_set_level(GPIO_NUM_17, 1);
     ESP_ERROR_CHECK(i2c_param_config(bus_num, &conf));
     ESP_ERROR_CHECK(i2c_driver_install(bus_num, I2C_MODE_MASTER, 0, 0, 0));
     
-    
-    while (i < 250) {
+    while (i < 2) {
+        ESP_ERROR_CHECK(readData( DataToRead, 4 ));
+        for (int k = 0; k < 4; k++ ) 
+            ESP_LOGI("READ: ", "%i, ", DataToRead[k]);
+        ESP_LOGI("TAG0: ", "%i \n", i++);  
+        vTaskDelay(pdMS_TO_TICKS(5000));   
+    }
 
-        ESP_ERROR_CHECK(sendData( DataToSend, len ));  // todo zprovoznit 
-        if (L_G) L_G = false; else  L_G = true;
-        gpio_set_level(GPIO_NUM_17, L_G);
-        for (int k = 0; k < 4; k++ ) DataToSend[k] += 1; 
-        for (int k = 0; k < 4; k++ ) ESP_LOGI("DATA: ", "%i, ", DataToSend[k]);
+    while (i < 250) {
+        ESP_ERROR_CHECK(sendData( DataToSend, len ));
+        for (int k = 0; k < 4; k++ ) 
+            DataToSend[k] += 1; 
+        for (int k = 0; k < 4; k++ ) 
+            ESP_LOGI("DATA: ", "%i, ", DataToSend[k]);
         ESP_LOGI("TAG1: ", "%i \n", i++);  
-        vTaskDelay(pdMS_TO_TICKS(2000));   
+        vTaskDelay(pdMS_TO_TICKS(5000));   
     }
 }
+
+
